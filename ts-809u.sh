8ca1b15ff68058075e8801b5cf9fd4eff890d2f1
svallero <svallero@to.infn.it>
2013-11-21 13:46:23 +0100

    Patches for QNAP

---
+++ ts-809u.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Patched variables for QNAP
+ADDRESS="192.168.1.215" #device is not on DNS
+export TARGET=$ADDRESS
+export TARGET_HOST=$ADDRESS
+export LUN="lun-0"
+export EXIT="yes"
+
